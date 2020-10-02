1d3ec1acf331bea5106cd18f13a658f949afa5ca
Ihar Berasneu <igor.simatic@gmail.com>
2020-10-02 10:49:04 +0300

    feat: add decision task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numberString = 0;
+    if (n < 0) {
+        numberString = String(-1 * n);
+    } else {
+        numberString = String(n);
+    }
+
+    let arrayNumber = numberString.split('');//
+    let reverseNumber = '';
+
+    for (let i = 1; i <= arrayNumber.length; i++) {
+        reverseNumber += arrayNumber[arrayNumber.length - i];
+    }
+    return reverseNumber;
+
+
+    //             if(n < 0) {
+    //                 numberString = String(-1 * n);
+    //             } else {
+    //                 numberString = String(n);
+    //             }
+    //             return numberString.split('').reverse().join('');
 }
